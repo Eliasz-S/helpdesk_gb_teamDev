5066a095ab378c20dc7b82f932721709733c550c
Elias <politico@mail.ru>
2021-09-22 22:26:13 +0300

    buefy table for users + edit user (unfinished)

--- resources/js/components/pages/users/Index.vue
+++ resources/js/components/pages/users/Index.vue
@@ -4,7 +4,7 @@
         <div class="card-header pb-0 d-flex justify-content-between">
             <h6>User List</h6>
         </div>
-        <div class="card-body px-0 pt-0 pb-2">
+        <!-- <div class="card-body px-0 pt-0 pb-2">
             <div class="table-responsive p-0">
                 <table class="table align-items-center mb-0">
                     <thead>
@@ -55,13 +55,167 @@
                     </tbody>
                 </table>
             </div>
-        </div>
+        </div> -->
+        <b-table
+            :data="users"
+            :paginated="isPaginated"
+            :per-page="perPage"
+            :current-page.sync="currentPage"
+            :pagination-simple="isPaginationSimple"
+            :pagination-position="paginationPosition"
+            :default-sort-direction="defaultSortDirection"
+            :pagination-rounded="isPaginationRounded"
+            :sort-icon="sortIcon"
+            :sort-icon-size="sortIconSize"
+            default-sort="user.first_name"
+            aria-next-label="Next page"
+            aria-previous-label="Previous page"
+            aria-page-label="Page"
+            aria-current-label="Current page">
+
+            <b-table-column field="id" label="#ID" width="40" sortable v-slot="props">
+                <div class="d-flex px-2 py-3">
+                    <p class="text-xs font-weight-bold mb-0">
+                        {{ props.row.id }}
+                    </p>
+                </div>
+            </b-table-column>
+
+            <b-table-column field="name" label="Login" sortable v-slot="props">
+                <div class="d-flex px-2 py-1">
+                    <div>
+                        <img :src="'../admin/img/team-3.jpg'" class="avatar avatar-sm me-3" alt="user1">
+                    </div>
+                    <div class="d-flex flex-column justify-content-center">
+                        <h6 class="mb-0 text-sm">{{ props.row.name }}
+                            <span v-if="props.row.first_name">
+                                | {{ props.row.first_name }} {{ props.row.last_name }}
+                            </span>
+                        </h6>
+                        <p class="text-xs text-secondary mb-0">{{ props.row.email }}</p>
+                    </div>
+                </div>
+            </b-table-column>
+
+            <b-table-column field="user_role.description" label="Role" sortable v-slot="props">
+                <div class="d-flex px-2 py-2">
+                    <span class="badge badge-sm bg-gradient-success">
+                        {{ props.row.user_role.description }}
+                    </span>
+                </div>
+            </b-table-column>
+
+            <b-table-column field="created_at" label="Date Of Addition" sortable v-slot="props">
+                <div class="d-flex px-2 py-3">
+                    <p class="text-xs font-weight-bold mb-0">
+                        {{ props.row.created_at | dateFormat }}
+                    </p>
+                </div>
+            </b-table-column>
+
+            <b-table-column>
+                <a href="javascript:;" class="text-secondary font-weight-bold text-xs" v-on:click="isComponentModalActive = true">
+                    <i class="fa fa-pencil-square-o sbadge badge-sm bg-gradient-primary color-white text-white px-1 rounded h6" aria-hidden="true"></i>
+                </a>
+                &nbsp; | &nbsp;
+                <a href="javascript:;" class="text-secondary font-weight-bold text-xs">
+                    <i class="fa fa-trash sbadge badge-sm bg-gradient-danger color-white text-white px-1 rounded h6" aria-hidden="true"></i>
+                </a>
+            </b-table-column>
+
+        </b-table>
     </div>
+    <section>
+        <!-- <b-button
+            label="Launch component modal"
+            type="is-primary"
+            size="is-medium"
+            @click="isComponentModalActive = true" /> -->
+
+        <b-modal
+            v-model="isComponentModalActive"
+            has-modal-card
+            trap-focus
+            :destroy-on-hide="false"
+            aria-role="dialog"
+            aria-label="Example Modal"
+            aria-modal>
+            <template #default="props">
+                <modal-form v-bind="formProps" @close="props.close"></modal-form>
+            </template>
+        </b-modal>
+    </section>
 </div>
 </template>
 
 <script>
 import axios from 'axios'
+
+var moment = require('moment')
+
+const ModalForm = {
+    props: ['email', 'password', 'canCancel'],
+    template: `
+        <form action="">
+            <div class="modal-card" style="width: auto">
+                <header class="modal-card-head">
+                    <p class="modal-card-title">Edit user data</p>
+                    <button
+                        type="button"
+                        class="delete"
+                        @click="$emit('close')"/>
+                </header>
+                <section class="modal-card-body">
+                    <b-field label="Login">
+                        <b-input
+                            type="text"
+                            :value="email"
+                            placeholder="{{ user.name }}"
+                            required>
+                        </b-input>
+                    </b-field>
+
+                    <b-field label="First Name">
+                        <b-input
+                            type="text"
+                            :value="email"
+                            placeholder="Your email"
+                            required>
+                        </b-input>
+                    </b-field>
+
+                    <b-field label="Last Name">
+                        <b-input
+                            type="text"
+                            :value="email"
+                            placeholder="Your email"
+                            required>
+                        </b-input>
+                    </b-field>
+
+                    <b-field label="Role">
+                        <b-input
+                            type="text"
+                            :value="password"
+                            password-reveal
+                            placeholder="Your password"
+                            required>
+                        </b-input>
+                    </b-field>
+
+                </section>
+                <footer class="modal-card-foot">
+                    <b-button
+                        label="Close"
+                        @click="$emit('close')" />
+                    <b-button
+                        label="Save"
+                        type="is-primary" />
+                </footer>
+            </div>
+        </form>
+    `
+}
 
 export default {
     data() {
@@ -69,10 +223,40 @@ export default {
             users: [],
             loading: true,
             errored: false,
+            //table
+            isPaginated: true,
+            isPaginationSimple: false,
+            isPaginationRounded: false,
+            paginationPosition: 'bottom',
+            defaultSortDirection: 'asc',
+            sortIcon: 'arrow-up',
+            sortIconSize: 'is-small',
+            currentPage: 1,
+            perPage: 5,
+            isComponentModalActive: false,
+            formProps: {
+                email: 'evan@you.com',
+                password: 'testing'
+            },
+            user: {
+                id: '',
+                name: '',
+                user_role: '',
+                created_at: '',
+                first_name: '',
+                last_name: ''
+            },
         }
     },
     mounted() {
         this.getUsers()
+    },
+    filters: {
+        dateFormat:
+            function(value) 
+            {
+                return moment(value).format('LLL');
+            }
     },
     methods: {
         getUsers() {
@@ -88,6 +272,9 @@ export default {
               })
               .finally(() => this.loading = false)
         }
+    },
+    components: {
+        ModalForm
     }
 }
 </script>
