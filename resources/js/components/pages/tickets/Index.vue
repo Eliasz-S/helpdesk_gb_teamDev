3ccfb4816a3e242c549af126286a7222efc9417d
nadjazahharovaee <alienlamahope@gmail.com>
2021-09-24 03:39:51 +0300

    A little changes in Tickets + loader

--- resources/js/components/pages/tickets/Index.vue
+++ resources/js/components/pages/tickets/Index.vue
@@ -1,4 +1,6 @@
 <template>
+    <section>
+      <b-loading :is-full-page="isFullPage" v-model="isLoading" :can-cancel="true"></b-loading>
       <div class="col-12">
         <div class="card mb-4">
             <div class="card-header pb-0 d-flex justify-content-between">
@@ -6,15 +8,6 @@
             </div>
             <div class="card-body px-0 pt-0 pb-2">
               <div class="table-responsive p-0">
-                
-                  <b-loading :is-full-page="isFullPage" v-model="isLoading" :can-cancel="false">
-                    <b-icon
-                      pack="fas"
-                      icon="sync-alt"
-                      size="is-large"
-                      custom-class="fa-spin">
-                    </b-icon>
-                  </b-loading>
                 <b-table
                     :data="tickets"
                     ref="table"
@@ -31,12 +24,11 @@
                     aria-page-label="Page"
                     aria-current-label="Current page"
                     class="table align-items-center mb-0">
-                <div class="" v-if="loading"> Loading...</div>
-                    <b-table-column field="id" label="ID" width="40" sortable numeric v-slot="props"  header-class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                    <b-table-column field="id" label="ID" width="40" numeric v-slot="props"  header-class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
                         {{ props.row.id }}
                     </b-table-column>
 
-                    <b-table-column field="name" label="Name" sortable v-slot="props" header-class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                    <b-table-column field="name" label="Name" sortable v-slot="props"  header-class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
                     <template v-if="showDetailIcon">
                           <div class="d-flex px-2 py-1">
                             <div>
@@ -85,34 +77,32 @@
                     <template #detail="props">
                         <article>
                             <div class="media" v-if="props.row.status === 'busy'">
-                            <figure class="media-left">
-                                <p class="image is-64x64">
-                                <img :src=" '../admin/img/team-3.jpg'" alt="user1" class="avatar avatar-sm me-3">
-                                </p>
-                            </figure>
                             <div class="media-content">
-                                <div class="content">
-                                    <div class="d-flex flex-column justify-content-center">
+                                  <div class="content row col-md-12">
+                                      <div class="d-flex flex-column justify-content-center col-md-3">
+                                        <img :src=" '../admin/img/team-3.jpg'" alt="user1" class="avatar avatar-sm me-3 mb-2">
                                         <h6 class="mb-0 text-sm">Staff: {{ props.row.staff_name }}</h6>
                                         <p class="text-xs text-secondary mb-0">{{ props.row.staff_email }}</p>
-                                    </div>
-                                    <p>
-                                        {{ props.row.message }}
-                                    </p>
-                                </div>
+                                      </div>
+                                      <div class="form-group col-md-9">
+                                        <textarea class="form-control" rows="3" :placeholder="props.row.message"></textarea>
+                                      </div>
+                                  </div>
                             </div>
                             </div>
                             <div class="media" v-else>
-                            <div class="media-content">
-                                <div class="content">
-                                    <div class="d-flex flex-column justify-content-center">
-                                        <h6 class="mb-0 text-sm">Staff: none</h6>
-                                    </div>
-                                    <p>
-                                        {{ props.row.message }}
-                                    </p>
-                                </div>
-                            </div></div>
+                              <div class="media-content">
+                                  <div class="content row col-md-12">
+                                      <div class="d-flex flex-column justify-content-center col-md-3">
+                                          <h6 class="mb-0 text-sm">Staff: none</h6>
+                                        <p class="text-xs text-secondary mb-0">Status: {{ props.row.status }}</p>
+                                      </div>
+                                      <div class="form-group col-md-9">
+                                        <textarea class="form-control" rows="3" :placeholder="props.row.message"></textarea>
+                                      </div>
+                                  </div>
+                              </div>
+                            </div>
                         </article>
                     </template>
                 </b-table>
@@ -120,6 +110,7 @@
             </div>
         </div>
       </div>
+    </section>
 </template>
 
 <script>
@@ -145,42 +136,9 @@ export default {
                 status: '',
                 message: '',
             },
-            columns: [
-                {
-                    field: 'id',
-                    label: 'ID',
-                    width: '40',
-                    numeric: true
-                },
-                {
-                    field: 'name',
-                    label: 'Name',
-                },
-                {
-                    field: 'subject',
-                    label: 'Subject',
-                },
-                {
-                    field: 'status',
-                    label: 'Status',
-                },
-                {
-                    field: 'priority',
-                    label: 'Priority',
-                },
-                {
-                    field: 'created_at',
-                    label: 'Date Of Addition',
-                },
-                {
-                    field: 'action',
-                    label: 'action',
-                },
-            ],
             ticket_id: '',
             pagination: {},
             edit: false,
-            loading: true,
             errored: false,
             defaultOpenedDetails: [1],
             showDetailIcon: true,
@@ -208,7 +166,7 @@ export default {
                 console.log(error)
                 this.errored = true
             })
-            .finally(() => this.loading = false, this.isLoading = false)
+            .finally(() => this.isLoading = false)
         },
     },
     computed: {
